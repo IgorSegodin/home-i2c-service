3fd3c765ede3b0bfc706e2352d2131f68ee37fc5
i.segodin <igor.segodin@gmail.com>
2019-11-09 13:59:14 +0200

    Automatic resolve public IP

--- src/main/java/org/isegodin/home/service/i2c/HomeI2cServiceApplication.java
+++ src/main/java/org/isegodin/home/service/i2c/HomeI2cServiceApplication.java
@@ -1,12 +1,13 @@
 package org.isegodin.home.service.i2c;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.commons.util.InetUtils;
-import org.springframework.cloud.commons.util.InetUtilsProperties;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaRegistration;
+import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaServiceRegistry;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.client.ClientResponse;
@@ -26,35 +27,41 @@ public class HomeI2cServiceApplication {
 
     /**
      * Resolve public IP
+     *
+     * @noinspection SpringJavaInjectionPointsAutowiringInspection
      */
-    @Bean
-    public InetUtils inetUtils(InetUtilsProperties inetUtilsProperties) {
+    @Autowired
+    public void overwriteInstanceIp(EurekaInstanceConfigBean configBean, EurekaServiceRegistry serviceRegistry, EurekaRegistration registration) {
         try {
-            ClientResponse ipResponse = WebClient.builder().build()
-                    .get()
-                    .uri("https://ifconfig.co/ip")
-                    .exchange()
-                    .block(Duration.ofSeconds(2));
-
-            String ip = Optional.ofNullable(ipResponse)
-                    .filter(r -> r.statusCode() == HttpStatus.OK)
-                    .map(r -> r.toEntity(String.class).block())
-                    .map(HttpEntity::getBody)
-                    .map(String::trim)
-                    .orElseThrow();
+            String ip = resolveExternalIp();
 
             log.info("Resolved external IP: " + ip);
 
-            inetUtilsProperties.setDefaultHostname(ip);
-            inetUtilsProperties.setDefaultIpAddress(ip);
+            configBean.setHostname(ip);
+            configBean.setIpAddress(ip);
+
+            serviceRegistry.register(registration);
+
+            System.out.println();
 
         } catch (Exception e) {
             log.warn("Failed to load external IP address", e);
         }
+    }
 
-        return new InetUtils(
-                inetUtilsProperties
-        );
+    private String resolveExternalIp() {
+        ClientResponse ipResponse = WebClient.builder().build()
+                .get()
+                .uri("https://ifconfig.co/ip")
+                .exchange()
+                .block(Duration.ofSeconds(2));
+
+        return Optional.ofNullable(ipResponse)
+                .filter(r -> r.statusCode() == HttpStatus.OK)
+                .map(r -> r.toEntity(String.class).block())
+                .map(HttpEntity::getBody)
+                .map(String::trim)
+                .orElseThrow();
     }
 
 }
